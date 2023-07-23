9f474cd25ff8f403a2d4a5c1c242128d30140d64
Mikeygoldman1 <goldmanmg@gmail.com>
2023-07-23 16:55:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template,request 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mikeygoldman1:Kolov1@localhost/vikings_norsemen_characters'
+db = SQLAlchemy(app)
+
+class Table1(db.Model):
+    __tablename__ = 'norsemen'
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String(2048))
+    character_description = db.Column(db.Text)
+    actor_name = db.Column(db.String(255))
+    actor_description = db.Column(db.Text)
+    
+
+class Table2(db.Model):
+    __tablename__ = 'viking'
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String(2048))
+    character_name = db.Column(db.String(255))
+    character_description = db.Column(db.Text)
+    actor_name = db.Column(db.String(255))
+    actor_description = db.Column(db.Text)
+    
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    norsemen_data = Table1.query.all()
+    viking_data = Table2.query.all()
+
+    # for row in norsemen_data:
+    #     if row.actor_name is None:
+    #         row.actor_name = row.character_description
+
+    # for row in viking_data:
+    #     if row.actor_name is None:
+    #         row.actor_name = row.character_name
+    
+    if request.method == 'POST':
+        table_search = request.form.get('table_search')
+        actor_search = request.form.get('actor_search')
+
+        if table_search:
+            if table_search.lower() == 'norsemen':
+                norsemen_data = Table1.query.all()
+                viking_data = []
+            elif table_search.lower() == 'viking':
+                viking_data = Table2.query.all()
+                norsemen_data = []
+        elif actor_search:
+            norsemen_data = Table1.query.filter(Table1.actor_name.contains(actor_search)).all()
+            viking_data = Table2.query.filter(Table2.actor_name.contains(actor_search)).all()
+
+    return render_template('index.html', norsemen_data=norsemen_data, viking_data=viking_data)
+    
+
+@app.route('/actor/<string:table>/<int:id>')
+def actor(table, id):
+    if table.lower() == 'norsemen':
+        actor = Table1.query.get(id)
+        return render_template('norsemen_actor.html', actor=actor)
+    else:
+        actor = Table2.query.get(id)
+        return render_template('viking_actor.html', actor=actor)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
