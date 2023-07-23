dd3bb6dcdfc583b5379d97feacd884e253290d3f
Mikeygoldman1 <88478063+Mikeygoldman1@users.noreply.github.com>
2023-07-23 18:50:09 +0200

    Add files via upload

--- pipelines.py
+++ pipelines.py
@@ -8,7 +8,7 @@
 from itemadapter import ItemAdapter
 
 
-class NorsemenscraperPipeline:
+class VikingscraperPipeline:
     def process_item(self, item, spider):
          # Check if the field is present
         if "actor_description" in item:
@@ -28,22 +28,21 @@ class NorsemenscraperPipeline:
         text = ' '.join(text.split())
         return text.replace('"', '').replace('[', '').replace(']', '')
         
-        
-    
+
 import psycopg2
-import os 
+import os
 class SaveToPostgreSQLPipeline:
 
     def __init__(self):
         DATABASE_URL = os.getenv('postgres://lucvierdfxortp:e91b0f5461b26645ae2dd72ab45ea1115e1b2924e2b26ddb4b96f6d5fbbc5832@ec2-35-169-9-79.compute-1.amazonaws.com:5432/df3uf8k36nralt')  # Get the Heroku Postgres URL from the environment variable
         self.conn = psycopg2.connect('postgres://lucvierdfxortp:e91b0f5461b26645ae2dd72ab45ea1115e1b2924e2b26ddb4b96f6d5fbbc5832@ec2-35-169-9-79.compute-1.amazonaws.com:5432/df3uf8k36nralt', sslmode='require')  # Connect to the Heroku Postgres database
         self.cur = self.conn.cursor()
-        # Drop the table if it exists
-        self.cur.execute("DROP TABLE IF EXISTS norsemen")
+        self.cur.execute("DROP TABLE IF EXISTS viking")
         self.cur.execute("""
-                         CREATE TABLE IF NOT EXISTS norsemen(
+                         CREATE TABLE IF NOT EXISTS viking(
                             id SERIAL PRIMARY KEY,
                             img_url VARCHAR(2048),
+                            character_name VARCHAR(255),
                             character_description TEXT,
                             actor_name VARCHAR(255),
                             actor_description TEXT
@@ -52,13 +51,14 @@ class SaveToPostgreSQLPipeline:
     
     def process_item(self,item,spider):
         self.cur.execute("""
-                         INSERT INTO norsemen( 
+                         INSERT INTO viking( 
                             img_url,
+                            character_name,
                             character_description,
                             actor_name,
                             actor_description
-                            ) VALUES ( %s, %s, %s, %s)""",
-                         ( item['img_url'], item['character_description'], item['actor_name'], item['actor_description']))
+                            ) VALUES ( %s, %s, %s, %s, %s)""",
+                         (item['img_url'],item['character_name'], item['character_description'], item['actor_name'], item['actor_description']))
         self.conn.commit()
         return item
 
